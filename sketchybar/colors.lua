44fbada5ca74385815f57fdedf6ab204cb626ef8
dengo123 <deniz060198@hotmail.com>
2025-01-03 22:34:51 +0100

    commit all

--- sketchybar/colors.lua
+++ sketchybar/colors.lua
@@ -1,28 +1,34 @@
+-- Colors for SketchyBar
+
 return {
-  black = 0xff181819,
-  white = 0xffe2e2e3,
-  red = 0xfffc5d7c,
-  green = 0xff9ed072,
-  blue = 0xff76cce0,
-  yellow = 0xffe7c664,
-  orange = 0xfff39660,
-  magenta = 0xffb39df3,
-  grey = 0xff7f8490,
-  transparent = 0x00000000,
+	black = "#FF2f383e",
+	white = "#FFd3c6aa",
+	grey = "#FF7e7666",
 
-  bar = {
-    bg = 0xf02c2e34,
-    border = 0xff2c2e34,
-  },
-  popup = {
-    bg = 0xc02c2e34,
-    border = 0xff7f8490
-  },
-  bg1 = 0xff363944,
-  bg2 = 0xff414550,
+	red = "#FFe67e80",
+	green = "#FFa7c080",
+	blue = "#FF7fbbb3",
+	yellow = "#FFdbbc7f",
+	orange = "#FFe69875",
+	magenta = "#FFd699b6",
+	cyan = "#FF83c092",
 
-  with_alpha = function(color, alpha)
-    if alpha > 1.0 or alpha < 0.0 then return color end
-    return (color & 0x00ffffff) | (math.floor(alpha * 255.0) << 24)
-  end,
+	bar = {
+		bg = "#FF22282c",
+		border = "#FF2f383e",
+	},
+	popup = {
+		bg = "#FF4e7357",
+		border = "#FF7e7666",
+	},
+
+	bg1 = "#FF4c706b",
+	bg2 = "#FFb2d6d1",
+
+	with_alpha = function(color, alpha)
+		if alpha > 1.0 or alpha < 0.0 then
+			return color
+		end
+		return (color & 0x00ffffff) | (math.floor(alpha * 255.0) << 24)
+	end,
 }
