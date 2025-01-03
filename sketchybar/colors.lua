53cf7b50b7f16ed5bcece2757b42673102402640
dengo123 <deniz060198@hotmail.com>
2025-01-03 23:23:17 +0100

    commit all

--- sketchybar/colors.lua
+++ sketchybar/colors.lua
@@ -1,29 +1,29 @@
 -- Colors for SketchyBar
 
 return {
-	black = "#FF2f383e",
-	white = "#FFd3c6aa",
-	grey = "#FF7e7666",
+	black = 0xff2f383e,
+	white = 0xffd3c6aa,
+	grey = 0xff7e7666,
 
-	red = "#FFe67e80",
-	green = "#FFa7c080",
-	blue = "#FF7fbbb3",
-	yellow = "#FFdbbc7f",
-	orange = "#FFe69875",
-	magenta = "#FFd699b6",
-	cyan = "#FF83c092",
+	red = 0xffe67e80,
+	green = 0xffa7c080,
+	blue = 0xff7fbbb3,
+	yellow = 0xffdbbc7f,
+	orange = 0xffe69875,
+	magenta = 0xffd699b6,
+	cyan = 0xff83c092,
 
 	bar = {
-		bg = "#FF22282c",
-		border = "#FF2f383e",
+		bg = 0xff22282c,
+		border = 0xff2f383e,
 	},
 	popup = {
-		bg = "#FF4e7357",
-		border = "#FF7e7666",
+		bg = 0xff4e7357,
+		border = 0xff7e7666,
 	},
 
-	bg1 = "#FF4c706b",
-	bg2 = "#FFb2d6d1",
+	bg1 = 0xff4c706b,
+	bg2 = 0xffb2d6d1,
 
 	with_alpha = function(color, alpha)
 		if alpha > 1.0 or alpha < 0.0 then
@@ -32,3 +32,4 @@ return {
 		return (color & 0x00ffffff) | (math.floor(alpha * 255.0) << 24)
 	end,
 }
+
